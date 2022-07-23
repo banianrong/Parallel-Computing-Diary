46e1e1f221a9adefc8cccebd075b0afc617801cc
banianrong <banianrong@163.com>
2022-07-23 17:18:46 +0800

    final

---
+++ Parallel_Code/OpenMP/lastprivate.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<omp.h>
+
+int main() {
+  int i = 10;
+  #pragma omp parallel for lastprivate(i)
+  for(int j = 0; j < 12; j++) {
+  	printf("Thread %d: i = %d\n", omp_get_thread_num(), i);
+  }
+  printf("i = %d\n", i);
+  return 0;
+}
